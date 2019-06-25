9aac1dce8c85b991d60b45442afcc657f22b307e
Guillaume Deleuze <deleuzegui@hotmail.com>
2019-06-25 11:17:13 +0200

    shameless part for item status

--- server/app/models/item.rb
+++ server/app/models/item.rb
@@ -2,4 +2,17 @@ class Item < ApplicationRecord
   belongs_to :product
 
   validates :product, presence: true
+
+
+# Si sélectionne robe rouge 
+
+# Je veux voir => Picked ? y / n
+# Si oui => par qui ? + checked ? y / n 
+# Si oui => par qui ? + packed ? y / n 
+# Si oui => par qui ?
+
+# Dans model items => 
+# ajouter « status » picked / checked / packed default 0 
+
+# implémenter fonction pour voir "qui" à effectuer l'action
 end
