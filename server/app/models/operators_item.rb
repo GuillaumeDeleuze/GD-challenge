359c1bb2e1db315d5b1493d047618546325b7039
Guillaume Deleuze <deleuzegui@hotmail.com>
2019-06-25 21:29:06 +0200

    remove action and use operator postes category instead

--- server/app/models/operators_item.rb
+++ server/app/models/operators_item.rb
@@ -2,6 +2,29 @@ class OperatorsItem < ApplicationRecord
   belongs_to :operator
   belongs_to :item
 
-  enum action: [:picked, :checked, :packed]
+  # enum action: %i(picked checked packed)
 
+  def picked?
+    operator.postes.last.category == 'pickup'
+  end
+  
+  def checked?
+    operator.postes.last.category == 'checkup'
+  end
+
+  def packed?
+    operator.postes.last.category == 'packup'
+  end
+
+  def action
+    if packed?
+      return 'packed'
+    elsif checked?
+      return 'checked'
+    elsif picked?
+      return 'picked'
+    else
+      return 'Aucune information'
+    end 
+  end
 end
--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -24,6 +24,43 @@ Product.create!([
 
 # Travailler le seed pour créer des items => les affilier à un opérateur 
 
+operator = 0
+poste = 0
+  3.times do 
+    operator += 1
+    poste += 1 
+    OperatorsPoste.create!(
+      operator_id: operator,
+      poste_id: poste
+    )
+  end
+  operator = 3
+  poste = 0
+  2.times do 
+    operator += 1
+    poste += 1 
+    OperatorsPoste.create!(
+      operator_id: operator,
+      poste_id: poste
+    )
+  end
+
+picker = []
+checker = [] 
+packer = []
+
+Operator.all.each do |operator|
+  if operator.postes.last.category == 'pickup'
+    picker << operator.id
+  end
+  if operator.postes.last.category == 'checkup'
+    checker << operator.id
+  end
+  if operator.postes.last.category == 'packup'
+    packer << operator.id
+  end
+end
+
 40.times do
   Item.create!(product_id: rand(1..4))
 end
@@ -33,7 +70,7 @@ puts 'create 30 picked items'
 30.times do
   item += 1 
   OperatorsItem.create!(
-    operator_id: rand(1..5), 
+    operator_id: picker.sample, 
     item_id: item,
     action: 0,
     done: true,
@@ -45,7 +82,7 @@ puts 'create 5 non picked items'
 5.times do
   item += 1 
   OperatorsItem.create!(
-    operator_id: rand(1..5), 
+    operator_id: picker.sample, 
     item_id: item,
     action: 0,
     )
@@ -56,11 +93,18 @@ item = 0
 20.times do
   item += 1 
   OperatorsItem.create!(
-    operator_id: rand(1..5), 
+    operator_id: checker.sample, 
     item_id: item,
     action: 1,
     done: true,
-    )
+  )
+  
+  OperatorsItem.create!(
+    operator_id: picker.sample, 
+    item_id: item,
+    action: 0,
+    done: true,
+  )
 end 
 
 puts 'create 10 unchecked items'
@@ -68,11 +112,18 @@ item = 20
 10.times do
   item += 1 
   OperatorsItem.create!(
-    operator_id: rand(1..5), 
+    operator_id: checker.sample, 
     item_id: item,
     action: 1,
+    done: false,
+  )
+
+  OperatorsItem.create!(
+    operator_id: picker.sample, 
+    item_id: item,
+    action: 0,
     done: true,
-    )
+  )
 end 
 
 puts 'create packed items'
@@ -80,9 +131,23 @@ item = 0
 20.times do
   item += 1 
   OperatorsItem.create!(
-    operator_id: rand(1..5), 
+    operator_id: packer.sample, 
     item_id: item,
     action: 2,
     done: [true, false].sample,
-    )
+  )
+
+  OperatorsItem.create!(
+    operator_id: checker.sample, 
+    item_id: item,
+    action: 1,
+    done: true,
+  )
+
+  OperatorsItem.create!(
+    operator_id: picker.sample, 
+    item_id: item,
+    action: 0,
+    done: true,
+  )
 end 
